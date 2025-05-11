47a7c92793d403296502ee7aca866f19ce1f258a
Michael Russell <m.mrussell@gmail.com>
2025-05-10 20:09:21 -0400

    Switch to Windy.com free weather map service

--- src/components/WeatherMap.jsx
+++ src/components/WeatherMap.jsx
@@ -6,7 +6,6 @@ const WeatherMap = () => {
   const lat = 27.6648;
   const lon = -81.5158;
   const zoom = 6;
-  const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY'; // You'll need to replace this with your actual API key
 
   return (
     <div className="weather-map-card">
@@ -17,7 +16,7 @@ const WeatherMap = () => {
           width="100%"
           height="300"
           frameBorder="0"
-          src={`https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=${lat}&lon=${lon}&zoom=${zoom}`}
+          src={`https://embed.windy.com/embed2.html?lat=${lat}&lon=${lon}&zoom=${zoom}&level=surface&overlay=temp&product=ecmwf&menu=&message=&marker=&calendar=&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1`}
           allowFullScreen
         />
       </div>
